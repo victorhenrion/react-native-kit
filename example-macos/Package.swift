6005efcc9fbe018ade515d89a1ce718934a81c0a
Victor <bonjour@victor.id>
2025-07-14 16:01:06 +0200

    macos: fix Debug not working due to missing CFBundleIdentifier - Add bundle ID otherwise React Inspector crashes without one, see https://github.com/microsoft/react-native-macos/blob/48e607706bfcc25b4c28eba1aa352f409c02975e/packages/react-native/React/Inspector/RCTCxxInspectorPackagerConnection.mm#L44 - Remove the Release bundle hack we don't need anymore

--- example-macos/Package.swift
+++ example-macos/Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ReactNativeKit",
-            path: "./ReactNative/macos/kit/Release/ReactNativeKit.xcframework"
+            path: "./ReactNative/macos/kit/Debug/ReactNativeKit.xcframework"
         ),
         .binaryTarget(
             name: "hermes",
-            path: "./ReactNative/macos/kit/Release/hermes.xcframework"
+            path: "./ReactNative/macos/kit/Debug/hermes.xcframework"
         ),
         .executableTarget(
             name: "Example",
@@ -29,6 +29,14 @@ let package = Package(
                 "ReactNativeKit",
                 "hermes",
             ],
+            linkerSettings: [
+                .unsafeFlags([
+                    "-Xlinker", "-sectcreate",
+                    "-Xlinker", "__TEXT",
+                    "-Xlinker", "__info_plist",
+                    "-Xlinker", "Sources/Example/Info.plist",
+                ])
+            ]
         ),
     ],
     swiftLanguageModes: [.v5]
--- example-macos/Sources/Example/ExampleApp.swift
+++ example-macos/Sources/Example/ExampleApp.swift
@@ -1,18 +1,10 @@
 import ReactNativeKit
 import SwiftUI
 
-// Debug releases don't work, this hack allows us to use dev bundle in Release
-class DevBundle: Bundle, @unchecked Sendable {
-    override func url(forResource name: String?, withExtension ext: String?) -> URL? {
-        return URL(string: "http://localhost:8081/index.bundle?platform=macos")!
-    }
-}
-
 @main
 struct ExampleApp: App {
     init() {
         ExpoBootstrap.registerModules()
-        ReactNativeBrownfield.shared.bundle = DevBundle()
         ReactNativeBrownfield.shared.startReactNative {
             print("React Native bundle loaded")
         }
