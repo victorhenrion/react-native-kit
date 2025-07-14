4cb504e5e0798d3c4ca3afdbcdfd96500bb24c5e
Victor <bonjour@victor.id>
2025-07-14 11:00:44 +0200

    macos: config/bundle hack

--- example-macos/Sources/Example/AppDelegate.swift
+++ example-macos/Sources/Example/AppDelegate.swift
@@ -1,11 +1,18 @@
 import ReactNativeKit
 import SwiftUI
 
+// Debug releases don't work, this hack allows us to use dev bundle in Release
+class DevBundle: Bundle, @unchecked Sendable {
+    override func url(forResource name: String?, withExtension ext: String?) -> URL? {
+        return URL(string: "http://localhost:8081/index.bundle?platform=macos")!
+    }
+}
+
 @main
 struct MyApp: App {
     init() {
         ExpoBootstrap.registerModules()
-        //ReactNativeBrownfield.shared.bundle = Bundle()
+        ReactNativeBrownfield.shared.bundle = DevBundle()
         ReactNativeBrownfield.shared.startReactNative {
             print("React Native bundle loaded")
         }
