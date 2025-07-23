bccf801f82e8ec4f3ca5efc73ed885458643cf67
Victor <bonjour@victor.id>
2025-07-23 12:54:44 +0200

    macos: fix check for expo import

--- packages/react-native-kit/macos/ReactNativeKit/ExpoBoostrap.swift
+++ packages/react-native-kit/macos/ReactNativeKit/ExpoBoostrap.swift
@@ -1,11 +1,19 @@
-@_implementationOnly import ExpoModulesCore
+#if canImport(ExpoModulesCore)
+    @_implementationOnly import ExpoModulesCore
 
-public enum ExpoBootstrap {
-    public static func registerModules() {
-        if let cls = NSClassFromString("ReactNativeKit.ExpoModulesProvider") as? ModulesProvider.Type {
-            AppContext().useModulesProvider(cls.init())
-        } else {
-            assertionFailure("ExpoModulesProvider not found in ReactNativeKit")
+    public enum ExpoBootstrap {
+        public static func registerModules() {
+            if let cls = NSClassFromString("ReactNativeKit.ExpoModulesProvider")
+                as? ModulesProvider.Type
+            {
+                AppContext().useModulesProvider(cls.init())
+            } else {
+                assertionFailure("ExpoModulesProvider not found in ReactNativeKit")
+            }
         }
     }
-}
+#else
+    public enum ExpoBootstrap {
+        public static func registerModules() {}
+    }
+#endif
