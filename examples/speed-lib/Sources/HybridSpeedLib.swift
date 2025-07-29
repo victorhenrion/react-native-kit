173ed03a0323b549746240c82dbb40b1f997b0b7
Victor <bonjour@victor.id>
2025-07-29 10:47:52 +0200

    fix "Could not find SpeedLibProvider" due to load order

--- examples/speed-lib/Sources/HybridSpeedLib.swift
+++ examples/speed-lib/Sources/HybridSpeedLib.swift
@@ -2,18 +2,16 @@ import Foundation
 import SpeedLibProtocol
 
 final class HybridSpeedLib: HybridSpeedLibSpec {
-    private let provider: SpeedLibProtocol
-
-    override init() {
+    private lazy var provider: SpeedLibProtocol = {
         guard
             let bundleName = Bundle.main.infoDictionary?["CFBundleName"],
             let providerClass = NSClassFromString("\(bundleName).SpeedLibProvider") as? SpeedLibProtocol.Type
         else {
-            fatalError("Could not find SpeedLibProvider class implementing SpeedLib protocol.")
+            fatalError("Could not find SpeedLibProvider class implementing SpeedLibProtocol.")
         }
 
-        self.provider = providerClass.init()
-    }
+        return providerClass.init()
+    }()
 
     func multiply(a: Double, b: Double) throws -> Double {
         return try provider.multiply(a: a, b: b)
